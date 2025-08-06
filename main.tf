3c3d041d122956be5d2257a5e5ba30b06e5ee56c
khbekzod <khabibullaevbekzodk@gmail.com>
2025-08-06 23:10:18 +0000

    added backend

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource aws_instance web {
     ami           = "ami-08ca1d1e465fbfe0c"
     instance_type = "t2.micro"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "kaizen-bekzodkh"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
+}
